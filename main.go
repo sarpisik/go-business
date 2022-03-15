64fa2cd3a16b8387b3b980d88ea544be1f12997e
Sarp ISIK <wdsarpisik@gmail.com>
2022-03-15 09:08:19 +0000

    Add template engine

--- main.go
+++ main.go
@@ -7,5 +7,28 @@ func main() {
 
 	app.Logger().SetLevel("debug")
 
+	// Init the handlebars engine
+	e := iris.Handlebars("./templates", ".html").Reload(true)
+
+	// Register a helper.
+	e.AddFunc("fullName", func(person map[string]string) string {
+		return person["firstName"] + " " + person["lastName"]
+	})
+
+	app.RegisterView(e)
+
+	app.Get("/", func(ctx iris.Context) {
+		viewData := iris.Map{
+			"author": map[string]string{"firstName": "Jean", "lastName": "Valjean"},
+			"body":   "Life is difficult",
+			"comments": []iris.Map{{
+				"author": map[string]string{"firstName": "Marcel", "lastName": "Beliveau"},
+				"body":   "LOL!",
+			}},
+		}
+
+		ctx.View("index.html", viewData)
+	})
+
 	app.Listen(":8080")
 }
